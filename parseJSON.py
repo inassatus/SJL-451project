52e483fc5b992a45af5802ab2c420e3b82a4d7cb
Lucas Lavine <sacul.lml@gmail.com>
2019-02-07 10:19:00 -0800

    Finished part 1, parsing the json files Not completely sure I did the checkin parsing correctly

--- parseJSON.py
+++ parseJSON.py
@@ -47,19 +47,82 @@ def parseBusinessData():
     f.close()
 
 def parseUserData():
-    #write code to parse yelp_user.JSON
-    pass
+    #read the JSON file
+	#all except compliment and elite
+    with open('.\yelp_user.JSON','r') as f:  #Assumes that the data files are available in the current directory. If not, you should set the path for the yelp data files.
+        outfile =  open('user.txt', 'w')
+        line = f.readline()
+        count_line = 0
+        #read each JSON abject and extract data
+        while line:
+            data = json.loads(line)
+            outfile.write(str(data['average_stars'])+'\t') #average stars
+            outfile.write(str(data['cool'])+'\t') #cool
+            outfile.write(str(data['fans'])+'\t') #fans
+            outfile.write(str([item for item in data['friends']])+'\t') #friends
+            outfile.write(str(data['funny'])+'\t') #funny
+            outfile.write(cleanStr4SQL(data['name']) + '\t')  #name
+            outfile.write(str(data['review_count'])+'\t') #review count
+            outfile.write(str(data['useful'])+'\t') #useful
+            outfile.write(cleanStr4SQL(data['user_id'])+'\t') #user id
+            outfile.write(cleanStr4SQL(data['yelping_since'])+'\t') #yelping since
+            outfile.write('\n')
+
+            line = f.readline()
+            count_line +=1
+    print(count_line)
+    outfile.close()
+    f.close()
 
 def parseCheckinData():
-    #write code to parse yelp_checkin.JSON
-    pass
+    #read the JSON file
+	#all keys
+    with open('.\yelp_checkin.JSON','r') as f:  #Assumes that the data files are available in the current directory. If not, you should set the path for the yelp data files.
+        outfile =  open('checkin.txt', 'w')
+        line = f.readline()
+        count_line = 0
+        #read each JSON abject and extract data
+        while line:
+            data = json.loads(line)
+            outfile.write(str(recursiveRemoval(data['time']))+'\t') #time
+            outfile.write(cleanStr4SQL(data['business_id'])+'\t') #business id
+            outfile.write('\n')
+
+            line = f.readline()
+            count_line +=1
+    print(count_line)
+    outfile.close()
+    f.close()
 
 
 def parseReviewData():
-    #write code to parse yelp_review.JSON
-    pass
+    #read the JSON file
+	#all keys
+    with open('.\yelp_review.JSON','r') as f:  #Assumes that the data files are available in the current directory. If not, you should set the path for the yelp data files.
+        outfile =  open('review.txt', 'w')
+        line = f.readline()
+        count_line = 0
+        #read each JSON abject and extract data
+        while line:
+            data = json.loads(line)
+            outfile.write(cleanStr4SQL(data['review_id'])+'\t') #review id
+            outfile.write(cleanStr4SQL(data['user_id'])+'\t') #user id
+            outfile.write(cleanStr4SQL(data['business_id'])+'\t') #business id
+            outfile.write(str(data['stars'])+'\t') #stars
+            outfile.write(cleanStr4SQL(data['date'])+'\t') #date
+            outfile.write(cleanStr4SQL(data['text']) + '\t') #text
+            outfile.write(str(data['useful'])+'\t') #useful
+            outfile.write(str(data['funny'])+'\t') #funny
+            outfile.write(str(data['cool'])+'\t') #cool
+            outfile.write('\n')
 
-parseBusinessData()
-parseUserData()
-parseCheckinData()
+            line = f.readline()
+            count_line +=1
+    print(count_line)
+    outfile.close()
+    f.close()
+
+#parseBusinessData()
+#parseUserData()
+#parseCheckinData()
 parseReviewData()
