0523ed23d383ec3f156afe9c98a78eb05f18b8f4
SHOYT0929 <39505984+SHOYT0929@users.noreply.github.com>
2019-02-07 23:11:57 -0800

    Add files via upload
    Part 3

---
+++ Milestone1/Milestone1/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Npgsql;
+
+namespace Milestone1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class Business
+        {
+            public string name { get; set; }
+            public string state { get; set; }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            addStates();
+            addColums2Grid();
+        }
+        private string buildConnString()
+        {
+            return "Host-localhost; Usersname = postgres; Password = 252100; Database = milestone1db";
+
+        }
+
+        public void addStates()
+        {
+            using (var comm = new NpgsqlConnection(buildConnString()))
+            {
+                comm.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = comm;
+                    cmd.CommandText = "SELECT distinct state FROM business ORDER BY state;";
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            stateList.Items.Add(reader.GetString(0));
+                        }
+                    }
+                }
+                comm.Close();
+            }
+        }
+
+        public void addColums2Grid()
+        {
+            DataGridTextColumn col1 = new DataGridTextColumn();
+            col1.Header = "Business Name";
+            col1.Binding = new Binding("name");
+            col1.Width = 255;
+            businessGrid.Columns.Add(col1);
+
+            DataGridTextColumn col2 = new DataGridTextColumn();
+            col2.Header = "State";
+            col1.Binding = new Binding("state");
+            businessGrid.Columns.Add(col2);
+
+
+            businessGrid.Items.Add(new Business() { name = "business1", state = "WA" });
+            businessGrid.Items.Add(new Business() { name = "business2", state = "CA" });
+            businessGrid.Items.Add(new Business() { name = "business3", state = "WA" });
+
+
+
+
+
+
+        }
+
+    }
+}
